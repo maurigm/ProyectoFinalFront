7faf0fc75ab330374a265bc9e7534e228598deae
Josei33 <jose.imhoff.20@gmail.com>
2023-09-03 21:42:02 -0300

    Creation of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ListProdutcs from "./Components/Page/Products/productcard";
 
 function App() {
   return (
     <>
+    <div>
+      <BrowserRouter>
+      <main>
+        <Routes>
+          <Route exact path="/product/card" element={<ListProdutcs />} />
       {/* aqui se van a subir los cambios anntes de enviarlos a produccion */}
+        </Routes>
+      </main>
+      </BrowserRouter>
+    </div>
     </>
   )
 }
---
+++ src/Components/Page/Products/productcard.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { CardGroup, Card, Pagination, Container, rol, Row } from "react-bootstrap";
+
+
+// const paginationBasic = (
+//     <div>
+//       <Pagination>{items}</Pagination>
+//       <br />
+  
+//       <Pagination size="lg">{items}</Pagination>
+//       <br />
+  
+//       <Pagination size="sm">{items}</Pagination>
+//     </div>
+//   );
+  
+//   render(paginationBasic);
+
+function ListProdutcs() {
+  return ( 
+    <Container>
+    <CardGroup>
+        <legend>Ultralivianos</legend>
+        <Row>
+      <Card className='col'>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>City</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card className='col'>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Frontier</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Horizon</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Soul</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Street</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      </Row>
+    </CardGroup>
+    </Container>
+  );
+}
+
+export default ListProdutcs;
