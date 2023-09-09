6bdb06bdf6e4b3319b6517afe59ccf53600b7b88
lautaroliendo <liendolautaro@gmail.com>
2023-09-09 06:06:52 -0300

    creando estructura de favPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ListProdutcs from "./Components/Page/Products/productcard";
 import Login from "./Components/Page/Session/login";
 import Register from "./Components/Page/Session/register"
 import { useState } from "react";
+import FavoriteProductsPage from "./Components/Page/fav/fav";
 
 function App() {
   const usuarioStorage = JSON.parse(sessionStorage.getItem('usuario')) || {}
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>} ></Route>
           <Route exact path="/register" element={<Register></Register>} ></Route>
           <Route exact path="/product/card" element={<ListProdutcs />} />
+          <Route exact path="/favorite" element={<FavoriteProductsPage />} />
           {/* <Route exact path="*" element={<Error404/>} /> */}
         </Routes>
       </main>
---
+++ src/Components/Page/fav/fav.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+
+function FavoriteProductsPage() {
+  // Supongamos que tienes una lista de productos favoritos en tu estado
+  const [favoriteProducts, setFavoriteProducts] = useState([
+    { id: 1, name: 'Product 1', description: 'Descripción del Producto 1' },
+    { id: 2, name: 'Product 2', description: 'Descripción del Producto 2' },
+    { id: 3, name: 'Product 3', description: 'Descripción del Producto 3' },
+    // Agrega más productos favoritos aquí
+  ]);
+
+  return (
+    <Container>
+      <h1>Mis Productos Favoritos</h1>
+      <Row>
+        {favoriteProducts.map((product) => (
+          <Col key={product.id} md={4} sm={6}>
+            <Card className="mb-3">
+              <Card.Body>
+                <Card.Img variant="top" src="src/public/assets/productoPrueba.png" />
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>{product.description}</Card.Text>
+                <Button variant="danger">Eliminar de Favoritos</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+export default FavoriteProductsPage;
