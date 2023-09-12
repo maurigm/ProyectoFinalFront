b4693fdc2b1f32abab955a636909fc53bb88488b
Sofia Lazarte <sofi.lazarte.sl@gmail.com>
2023-09-12 18:24:09 -0300

    front de pagina principal, se agrega carpeta assets con las imagenes y se agrega pagania de nosotros y contacto

--- src/Components/layout/Nav.jsx
+++ src/Components/layout/Nav.jsx
@@ -5,7 +5,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import '@fortawesome/fontawesome-free/css/all.css';
 import LoginModal from '../Page/Session/login';
 import RegisterModal from '../Page/Session/register';
-
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 
 
@@ -13,14 +14,10 @@ function BasicExample() {
   return (
     <Navbar expand="lg" bg="dark" variant="dark" fixed="top">
     <Container>
-      <Navbar.Brand href="#">
-        <img
-          alt=""
-          src="/Img/Cards/navbar.png"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
+      <Navbar.Brand>
+        <Link to="/">
+          <img src={logo} alt="logo" width="100" />
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -29,7 +26,7 @@ function BasicExample() {
         </Nav>
         <Nav>
           <Nav.Link href="contacto.html">Contacto</Nav.Link>
-          <Nav.Link href="#"><i className="fas fa-search"></i></Nav.Link>
+          <Nav.Link href="#">Nosotros<i className="fas fa-search"></i></Nav.Link>
           <Nav.Link href="#"><i className="fas fa-star"></i></Nav.Link>
           <Nav.Link href="#"><i className="fas fa-shopping-cart"></i></Nav.Link>
           <LoginModal/>
