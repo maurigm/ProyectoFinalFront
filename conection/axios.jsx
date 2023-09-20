1c33e71f50fd581197a2c5f9a20b798f74a0ed7d
Josei33 <jose.imhoff.20@gmail.com>
2023-09-19 23:03:43 -0300

    Conexion de base datos con axios

---
+++ conection/axios.jsx
@@ -0,0 +1,11 @@
+import  axios  from "axios";
+const url = import.meta.env.VITE_URL_BACKEND;
+console.log(url);
+
+const instance = axios.create({
+    baseURL: url ? url: 'https://localhost:3010/api',
+    timeout: 2000,
+    headers: {'content-type': 'application/json'}
+  });
+
+export default instance
